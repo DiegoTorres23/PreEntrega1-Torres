aa63bcb036ba0eeff79c4b6fea8b5b4b875e9058
Diego Torres <diegodavidtorres22@gmail.com>
2024-02-26 18:00:15 -0300

    carrito correcto

--- js/index.js
+++ js/index.js
@@ -14,15 +14,6 @@ const obtenerCarritoLS=()=>{
   return JSON.parse(localStorage.getItem("carrito")) || []
 }
 
-const totalProductos=()=>{
-  const carrito=carrito.length
-  return carrito.length;
-}
-
-const logoCarrito=()=>{
-  document.getElementById("carritoImg").innerHTML=totalProductos()
-}
-
 const obtenerIdProductoLS=()=>{ 
   return JSON.parse(localStorage.getItem("pez")) || 0;
 }
@@ -30,46 +21,39 @@ const obtenerIdProductoLS=()=>{
 const guardarProducto = (id) => {
   localStorage.setItem("pez", JSON.stringify(id))
   if (id) {
-let carro = []
-    carro = obtenerCarritoLS()
-    console.log(carro);
+    let carrito = []
+    carrito = obtenerCarritoLS()
+    console.log(carrito);
     let id = obtenerIdProductoLS()
     console.log(id);
     const peces = obtenerProductosLS()
     console.log(peces);
     let producto = peces.find(item => item.id === id)
     console.log(producto)
-    carro.push(producto)
-    pushCarritoLS(carro)
+    carrito.push(producto)
+    pushCarritoLS(carrito)
+    renderNumCarrito()
   }
-
 }
 
-const agregarProductoCarrito =()=>{
-carrito= obtenerCarritoLS();
-pushCarritoLS(carrito)
+const totalProductos=()=>{
+  let carrito=obtenerCarritoLS()
+  carrito=carrito.length
+  return carrito;
 }
-const renderCardsProductos=()=>{
-  const peces= obtenerProductosLS()
-  let cards="";
-  for (const producto of peces) {
-    cards+=
-    `<div class= "pCard card m-4" style="width: 22rem; height: auto;">
-    <img src="${producto.imagen}" class="img-fluid" alt="img-pez-xanthurum">
-    <div class="card-body">
-      <h5 class="pCardTitle card-title">${producto.Nombre}</h5>
-      <hr>
-      <p class="card-text">Habitat: ${producto.Habitat}</p>
-      <p class="card-text">Tamaño: ${producto.Tamaño}</p>
-      <p class="card-text">Acuarios: ${producto.Acuarios}</p>
-      <p class="card-text">Precio: <b>$${producto.precio}</b></p>
-      <button onclick="guardarProducto(${producto.id})" class="pBtn btn btn-info">Agregar al Carrito<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="ms-2 bi bi-cart3" viewBox="0 0 16 16">
-        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-      </svg></button>
-    </div>
-  </div>`
-  }
-  document.getElementById("containerProductos").innerHTML=cards
+
+const renderNumCarrito=()=>{
+  document.getElementById("carritoImg").innerHTML=totalProductos()
 }
-pushProductosLS(peces)
-renderCardsProductos()
+
+const montoPagar=()=>{
+  let suma=obtenerCarritoLS()
+  return suma.reduce((iterador,item)=>iterador+=item.precio,0)
+}
+
+const eliminarProductoCarrito=(id)=>{
+  const carrito=obtenerCarritoLS()
+  const nuevoCarrito=carrito.filter(item => item.id != id)
+  pushCarritoLS(nuevoCarrito)
+  renderCarrito()
+}
--- js/render.js
+++ js/render.js
@@ -1,17 +1,24 @@
-const renderCarrito=()=>{
-    const peces= obtenerCarritoLS()
-    let contenido="";
-    for (const produ of peces) { obe
-      contenido+=
-      `<div class="card" style="width: 18rem;">
-      <img src="${produ.imagen} " class="card-img-top" alt="...">
+const renderCardsProductos=()=>{
+    const peces= obtenerProductosLS()
+    let cards="";
+    for (const producto of peces) {
+      cards+=
+      `<div class= "pCard card m-4" style="width: 22rem; height: auto;">
+      <img src="${producto.imagen}" class="img-fluid" alt="img-pez-xanthurum">
       <div class="card-body">
-        <p class="card-text">${produ.Nombre} </p>
+        <h5 class="pCardTitle card-title">${producto.Nombre}</h5>
+        <hr>
+        <p class="card-text">Habitat: ${producto.Habitat}</p>
+        <p class="card-text">Tamaño: ${producto.Tamaño}</p>
+        <p class="card-text">Acuarios: ${producto.Acuarios}</p>
+        <p class="card-text">Precio: <b>$${producto.precio}</b></p>
+        <button onclick="guardarProducto(${producto.id})" class="pBtn btn btn-info">Agregar al Carrito<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="ms-2 bi bi-cart3" viewBox="0 0 16 16">
+          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+        </svg></button>
       </div>
     </div>`
     }
-    document.getElementById("productosCarrito").innerHTML=contenido;
-  }
-  pushCarritoLS(guardarProducto())
-  renderCarrito()
-  
+    document.getElementById("containerProductos").innerHTML=cards
+}
+  pushProductosLS(peces)
+  renderCardsProductos()
